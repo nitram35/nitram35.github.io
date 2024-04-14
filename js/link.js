5c5204b6f8dfbca02c24281cf1e510111d9975bd
Martin Gandon <martin.gandon@free.fr>
2024-04-14 20:36:22 +0200

    adding project1 img

---
+++ js/link.js
@@ -0,0 +1,35 @@
+function sendEmail() {
+    var link = document.createElement("a")
+    link.href = "mailto:martin.gandon@free.fr"
+    link.target = "_blank"
+    link.click()
+}
+
+function openLinkedIn() {
+    var link = document.createElement("a")
+    link.href = "https://www.linkedin.com/in/martingandon/"
+    link.target = "_blank"
+    link.click()
+}
+
+function openGitHub() {
+    var link = document.createElement("a")
+    link.href = "https://github.com/nitram35"
+    link.target = "_blank"
+    link.click()
+}
+
+function openCV() {
+    var link = document.createElement("a")
+    link.href = ""
+    link.target = "_blank"
+    link.click()
+}
+
+function project1Demo() {
+    var link = document.createElement("a")
+    link.href = "https://nitram35.github.io/nitram35.github.io/"
+    link.target = "_blank"
+    link.click()
+}
+
